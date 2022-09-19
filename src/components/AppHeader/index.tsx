8e19e4f508e5ac000440df06955f1cd65f330e0e
Aleksey Stepanov <ab.stepanov@bpcbt.com>
2022-09-20 01:50:07 +0400

    Refactors

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,18 +1,12 @@
-import React, { useState } from 'react';
-import {
-  Link,
-} from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export const AppHeader = ({ handleGridOpen, handleGanttOpen }) => {
-  return (
-    <header className='flex flex-col items-start justify-center p-10 gap-10 h-[20vh] bg-slate-500'>
-      <h1 className='flex font-semibold text-3xl text-white fo'>Планировщик участка 4.1 Потока МПГ</h1>
-      <div className='flex gap-2'>
-        <Link className='button' to="/">Выбор партий</Link>
-        <Link className='button' to="/planner">План производства</Link>
-        {/* <button className='button' onClick={handleGridOpen}>Выбор партий</button> */}
-        {/* <button className='button' onClick={handleGanttOpen}>План производства</button> */}
-      </div>
-    </header>
-  )
-}
+export const AppHeader = () => (
+  <header className='flex flex-col items-start justify-center p-10 gap-10 h-[20vh] bg-slate-500'>
+    <h1 className='flex font-semibold text-3xl text-white fo'>Планировщик участка 4.1 Потока МПГ</h1>
+    <div className='flex gap-2'>
+      <Link className='button' to="/">Выбор партий</Link>
+      <Link className='button' to="/planner">План производства</Link>
+    </div>
+  </header>
+)
--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -23,68 +23,16 @@ import './index.css';
 import { StatusBar } from '../StatusBar';
 
 function App() {
-  const [activeView, setActiveView] = useState<'grid' | 'gantt'>('grid');
-  const data = useSelector(({ data }: { data: IDataState }) => data.value);
-
-  const mapDataToTasks = () => (
-    data.map(d => ({
-      id: d.id,
-      name: d.nomenclature.name,
-      start: new Date(),
-      end: addDays(new Date(), 10),
-      progress: 0,
-    // custom_class?: string;
-    // setDependencies(value: string[]): void;
-    // dependencies: string;
-    }))
-  )
-  
- 
-  const handleGridOpen = () => {
-    setActiveView('grid');
-  }
-
-  const handleGanttOpen = () => {
-    setActiveView('gantt');
-  }
-
-  const handleImportFile = (e) => {
-    console.log(e);
-  }
 
   return (
     <BrowserRouter>
       <div className="app-container">
-        <AppHeader
-            handleGridOpen={handleGridOpen}
-            handleGanttOpen={handleGanttOpen}
-        />
+        <AppHeader />
         <main className='flex flex-col flex-1 w-[100%]'>
-          <div className='flex flex-col'>
-            <Routes>
-              <Route path="/" element={<HomeScreen />} />
-              <Route path="/planning" element={<PlanningScreen />} />
-            </Routes>
-          </div>
-          {/* <div className={clsx(['flex', 'flex-col'],{
-            hidden: activeView !== 'grid'
-          })}>
-            <Grid />
-          </div>
-          <div className={clsx({
-            hidden: activeView !== 'gantt'
-          })}>
-            {data && data.length && (
-              <FrappeGantt
-                tasks={mapDataToTasks()}
-                viewMode={ViewMode.Day}
-                onClick={task => console.log(task)}
-                onDateChange={(task, start, end) => console.log(task, start, end)}
-                onProgressChange={(task, progress) => console.log(task, progress)}
-                onTasksChange={tasks => console.log(tasks)}
-              />
-            )}
-          </div> */}
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/planning" element={<PlanningScreen />} />
+          </Routes>
         </main>
         <StatusBar />
       </div>
