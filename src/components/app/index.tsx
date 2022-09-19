fafdaa183032781d00ca5d62f4fc88535c8c39c7
Aleksey Stepanov <ab.stepanov@bpcbt.com>
2022-09-20 01:51:57 +0400

    Refactors

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,29 +1,14 @@
-import React, { useState } from 'react';
-import { clsx } from 'clsx';
-import { FrappeGantt, ViewMode } from 'frappe-gantt-react';
-import { AppHeader } from '../AppHeader';
-import addDays from 'date-fns/addDays'
-
-import Grid from '../Grid';
-
-// import DownloadIcon from '../../img/icons/download.svg';
-import { useSelector } from 'react-redux';
-import { IDataState } from '../../store/dataSlice';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { HomeScreen, PlanningScreen } from '../../screens/';
 
-
-import './index.css';
+import { AppHeader } from '../AppHeader';
 import { StatusBar } from '../StatusBar';
 
-function App() {
+import './index.css';
 
+function App() {
   return (
     <BrowserRouter>
       <div className="app-container">
