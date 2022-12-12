9c8ecfe7c9d4332df85d776f30a9043640529b67
lorentzimys <lorentzimys@yandex.ru>
2022-12-12 22:13:49 +0400

    Fixed build errors on Windows OS

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '@/store';
-import App from '.';
+import App from '@components/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
